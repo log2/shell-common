4bc7a0dd27c45ae9cbb796df9743b390f42e1b2d
Antonio Locane <a.locane@eco-mind.eu>
2020-10-02 12:21:51 +0200

    improved install mechanism

--- bootstrap/dep-bootstrap.sh
+++ bootstrap/dep-bootstrap.sh
@@ -1,7 +1,46 @@
 #!/usr/bin/env bash
 
-scriptName=$(basename "$0")
+basherExecutable="${BASHER_ROOT:-$HOME/.basher}/bin/basher"
+shellName="${SHELL##*/}"
+
+if [[ $1 == "install" ]] ; then
+    if [ -f "$basherExecutable" ]; then
+        >&2 echo "Basher executable already present, skipping installation"
+    else
+        >&2 echo "Basher executable not found, cloning git repo..."
+        git clone https://github.com/basherpm/basher.git "$HOME"/.basher
+        case $shellName in
+            "bash")
+                rcFile=".bashrc"
+                ;;
+            "dash")
+                rcFile=".bashrc"
+                ;;
+            "sh")
+                rcFile=".profile"
+                ;;
+            "zsh")
+                rcFile=".zshrc"
+                ;;
+            *)
+                >&2 echo "Unsupported shell $shellName"
+                exit 1
+                ;;
+        esac
+        # shellcheck disable=SC2016
+        rcFileLine1='export PATH="'$(dirname "$basherExecutable")':$PATH"'
+        echo "$rcFileLine1" >> "$HOME"/$rcFile
+        # shellcheck disable=SC2016
+        rcFileLine2='eval "$('$basherExecutable' init - '$shellName')"'
+        echo "$rcFileLine2" >> "$HOME"/$rcFile
+        >&2 echo "Basher installation completed"
+    fi
+    exit
+fi
+
+scriptName=$(basename "${BASH_SOURCE[0]}")
 scriptVersion=0.1.0-SNAPSHOT
+>&2 echo "Running $scriptName version=$scriptVersion"
 
 if [[ $DEP_SOURCED == 1  ]]; then 
     >&2 echo "Error invoking '$scriptName' (already sourced)"
@@ -13,15 +52,13 @@ if [[ -z $DEP_CALLER_ID ]] ; then
     exit 1
 fi
 
->&2 echo -n "Running '$scriptName' version '$scriptVersion'. Checking basher installation... "
-basherInitFile="${BASHER_ROOT:-$(dirname "$(command -v basher)")/..}/lib/include.${SHELL##*/}"
-if [ -f "$basherInitFile" ]; then
-    # shellcheck disable=SC1090,SC1091
-    source "$basherInitFile"
+if [ -f "$basherExecutable" ]; then
+    >&2 echo "Detected shell=$shellName basher=$basherExecutable"
+    # shellcheck disable=SC1090
+    source "$(dirname "$basherExecutable")/../lib/include.$shellName"
     DEP_SOURCED=1
-    >&2 echo "OK"
 else
-    >&2 echo "required basher init script not found" 
+    >&2 echo "Unable to find basher executable, try installing using command: '$scriptName install'"
     exit 1
 fi
 
@@ -41,9 +78,34 @@ callerPath=${DEP_CALLER_PATH:-"basherTemp"}
 callerID=$DEP_CALLER_ID
 checkBlanks "$callerID" "$repoBaseURL" "$callerPath"
 
-basherInclude() {
+dep() {
+    echo "$PATH"
+    tail -n 5 "$HOME"/.bashrc
+    which basher
+
+    command=$1
+    if [[ -z $command ]] ; then
+        >&2 echo "usage: dep <command> <options> (currently available commands: [include])"
+        exit 1
+    fi
+    shift
+    "dep_$command" "$@"
+}
+
+dep_include() {
+    packageName=$1
+    packageTag=$2
+    scriptName=$3
+    if [[ -z $packageName ]] || [[ -z $packageTag ]] || [[ -z $scriptName ]] ; then
+        >&2 echo "usage: dep include <packageName> <packageTag> <scriptName>"
+        exit 1
+    fi
+    checkBlanks "$packageName" "$packageTag" "$scriptName"
+    local logSubstring="script '$scriptName.sh' tag=$packageTag git repo=$repoBaseURL/$packageName callerID=$callerID callerPath=$callerPath"
+    >&2 echo "including $logSubstring"
+
     callerPackageName="$packageName-(tag-$packageTag-includedBy-$callerID)"
-    if basher list | grep -q "$callerPackageName" && [[ -d "$callerPath/$packageName" ]] ; then
+    if $basherExecutable list | grep -q "$callerPackageName" && [[ -d "$callerPath/$packageName" ]] ; then
         gitExecute="git --git-dir "$callerPath/$packageName/.git""
         existingTag=$($gitExecute describe --exact-match --tags) || exit 1
         >&2 echo "found existingTag: $existingTag"
@@ -55,33 +117,10 @@ basherInclude() {
         [ ! -d "$callerPath" ] && mkdir -p "$callerPath"
         rm -rf "./$callerPath/$packageName"
         git clone --depth 1 --branch "$packageTag" "$repoBaseURL/$packageName" "$callerPath/$packageName" || exit 1
-        basher link "$callerPath/$packageName" "$callerPackageName"
+        $basherExecutable link "$callerPath/$packageName" "$callerPackageName" || exit 1
     fi
 
-    CALLER_PACKAGE=$callerPackageName include "$callerPackageName" "lib/$scriptName.sh"
-}
+    CALLER_PACKAGE=$callerPackageName include "$callerPackageName" "lib/$scriptName.sh" || exit 1
 
-dep() {
-    command=$1
-    shift
-    case $command in
-        include)
-            packageName=$1
-            packageTag=$2
-            scriptName=$3
-            if [[ -z $packageName ]] || [[ -z $packageTag ]] || [[ -z $scriptName ]] ; then
-                >&2 echo "usage: dep include <packageName> <packageTag> <scriptName>"
-                exit 1
-            fi
-            checkBlanks "$packageName" "$packageTag" "$scriptName"
-            local logSubstring="script '$scriptName.sh' tag=$packageTag git repo=$repoBaseURL/$packageName callerID=$callerID callerPath=$callerPath"
-            >&2 echo "including $logSubstring"
-            basherInclude "$@"
-            >&2 echo "inclued $logSubstring"
-            ;;
-        * ) 
-            # implement extensibility
-            >&2 echo "usage: dep <command> <options> (currently available commands: [include])"
-            exit 1
-    esac
+    >&2 echo "inclued $logSubstring"
 }
